1263780bd5c5135f11be0de1160a6a9bb3a64141
Silver Kirotar <sillu012@hotmail.com>
2019-01-07 03:06:26 +0200

    Initial Menu controllers for testing

---
+++ Assets/Scripts/UI/Menu/BackToMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenuController : MonoBehaviour
+{
+    private void Update()
+    {
+        if ( Input.GetButtonDown( "Cancel" ) )
+        {
+            SceneManager.LoadScene( "Menu" );
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private void Update()
+    {
+        if ( Input.GetButtonDown( "Submit" ) )
+        {
+            LoadMainScene();
+        }
+        else if ( Input.GetButtonDown( "Cancel" ) )
+        {
+            Quit();
+        }
+    }
+
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene( "Main" );
+    }
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
