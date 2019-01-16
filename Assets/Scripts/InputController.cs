72d5117b221259a4ef5e16bdb25c770da0019077
Silver Kirotar <sillu012@hotmail.com>
2019-01-16 22:03:59 +0200

    gg

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private float _walkingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        var vertical = Vector3.up * GetAxisMovement( "Vertical" );
+        var horizontal = Vector3.right * GetAxisMovement( "Horizontal" );
+        var movement = ( vertical + horizontal );
+        movement = movement.magnitude > 1 ? movement : ( movement / movement.magnitude );
+        movement *= _walkingSpeed * Time.deltaTime;
+
+        if ( movement.magnitude > 0 )
+        {
+            transform.position += movement;
+        }
+
+        if ( Input.GetButtonUp( "Inventory" ) )
+        {
+            GetComponent<InventoryController>().ShowCanvas();
+        }
+    }
+
+    private static float SignedSquare( float value )
+    {
+        return value >= 0 ? Mathf.Pow( value, 8 ) : -Mathf.Pow( value, 8 );
+    }
+
+    private static float GetAxisMovement( string axis )
+    {
+        return ( Input.GetButton( axis ) ? Input.GetAxis( axis ) : SignedSquare( Input.GetAxis( axis ) ) * 0.5f );
+    }
+}
