2a4e3878b3c171ff97ccf0ad7720a3952bc84c2e
Silver Kirotar <sillu012@hotmail.com>
2019-01-16 22:02:46 +0200

    Simple input controller for player + controller for chest and inv

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField] private GameObject _imsCanvasObj;
+    [SerializeField] private GameObject _chestCanvasObj;
+
+    private void Start()
+    {
+        ShowChest( false );
+    }
+
+    public void ShowCanvas()
+    {
+        _imsCanvasObj.SetActive( !_imsCanvasObj.activeSelf );
+    }
+
+    private void OnTriggerEnter2D( Collider2D other )
+    {
+        Debug.Log( other.name );
+        ShowChest( true );
+    }
+
+    private void OnTriggerExit2D( Collider2D other )
+    {
+        ShowChest( false );
+    }
+
+    private void ShowChest( bool show )
+    {
+        _imsCanvasObj.SetActive( show );
+        _chestCanvasObj.SetActive( show );
+    }
+}
