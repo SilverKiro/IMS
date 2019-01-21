80a8b8dc0bf21868606c2c59106a5cf5733abc08
Alandur <alan.durnev@gmail.com>
2019-01-21 23:05:42 +0200

    prefabs

---
+++ Assets/ItemDB.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    [SerializeField] private Category _category;
+    [SerializeField] private int price;
+    [SerializeField] private int weight;
+    [SerializeField] private string name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private enum Category
+    {
+        MONEY,
+        QUEST_ITEM,
+        COMMON,
+        EXOTIC,
+        LEGENDARY
+    }
+}
